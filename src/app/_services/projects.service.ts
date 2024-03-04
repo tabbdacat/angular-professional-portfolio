bd041e972b745fa7f64370e846d62cff2b4af974
Meow <tabbathapischke@gmail.com>
2024-03-04 09:41:01 -1000

    add service to import projects array, add method to generate projects

---
+++ src/app/_services/projects.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+
+  projects: Project[] = [
+    {
+        id: 0,
+        name: "Culinary Connect - Group Collaboration",
+        pictures: [], 
+        // githubLink: "https://github.com/quar-tech/mvc-recipe-sharing",
+        projectLink: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/",
+        summary: "Brief summary",
+        description: "Full Description of Project",
+        tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    },
+    {
+      id: 1,  
+      name: "MovieMix & Sip - Group Collaboration",
+        pictures: [], 
+        // githubLink: "https://github.com/ucb-allstars-project-1/movie-cocktail-combo",
+        projectLink: "https://ucb-allstars-project-1.github.io/movie-cocktail-combo/",
+        summary: "Brief summary",
+        description: "Full Description of Project",
+        tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    },
+    {
+      id: 2,
+        name: "Tech Blog",
+        pictures: [], 
+        // githubLink: "https://github.com/tabbdacat/tech-blog",
+        projectLink: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/",
+        summary: "Brief summary",
+        description: "Full Description of Project",
+        tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    },
+    {
+      id: 3,
+        name: "Weather Dashboard",
+        pictures: [], 
+        // githubLink: "https://github.com/tabbdacat/weather-dashboard",
+        projectLink: "https://tabbdacat.github.io/weather-dashboard/",
+        summary: "Brief summary",
+        description: "Full Description of Project",
+        tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    },
+    {
+      id: 4,
+        name: "Coding Quiz",
+        pictures: [], 
+        // githubLink: "https://github.com/tabbdacat/coding-quiz",
+        projectLink: "https://tabbdacat.github.io/coding-quiz/",
+        summary: "Brief summary",
+        description: "Full Description of Project",
+        tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    },
+    {
+      id: 5,
+        name: "Social Network API",
+        pictures: [],
+        // githubLink: "https://github.com/tabbdacat/social-network-api",
+        projectLink: "https://drive.google.com/file/d/1xwfROda1DgFF3f8CBdpaUapTH7MgSlPs/view",
+        summary: "Brief summary",
+        description: "Full Description of Project",
+        tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    },
+  ];
+
+  GetProjects() {
+    return this.projects;
+  }
+
+  GetProjectById(id: number) : Project {
+    let project = this.projects.find(project => project.id === id);
+
+    if (project === undefined) {
+      throw new TypeError('There is no project that matches the id:' + id)
+    }
+
+    return project;
+  }
+
+  constructor() { }
+}
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 import { Tag } from '../_models/Tag'
+import { ProjectsService } from '../_services/projects.service';
 // import { culinaryConnectImg } from '../../assets/culinaryConnectImg.jpg';
 // import { weatherDashImg } from '../../assets/weatherDashImg.jpg';
 // import { techBlogImg } from '../../assets/techBlogImg';
@@ -14,72 +15,14 @@ import { Tag } from '../_models/Tag'
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
 
-  projects: Project[] = [
-      {
-          id: 0,
-          name: "Culinary Connect - Group Collaboration",
-          pictures: [], 
-          // githubLink: "https://github.com/quar-tech/mvc-recipe-sharing",
-          projectLink: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/",
-          summary: "Brief summary",
-          description: "Full Description of Project",
-          tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-      },
-      {
-        id: 1,  
-        name: "MovieMix & Sip - Group Collaboration",
-          pictures: [], 
-          // githubLink: "https://github.com/ucb-allstars-project-1/movie-cocktail-combo",
-          projectLink: "https://ucb-allstars-project-1.github.io/movie-cocktail-combo/",
-          summary: "Brief summary",
-          description: "Full Description of Project",
-          tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-      },
-      {
-        id: 2,
-          name: "Tech Blog",
-          pictures: [], 
-          // githubLink: "https://github.com/tabbdacat/tech-blog",
-          projectLink: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/",
-          summary: "Brief summary",
-          description: "Full Description of Project",
-          tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-      },
-      {
-        id: 3,
-          name: "Weather Dashboard",
-          pictures: [], 
-          // githubLink: "https://github.com/tabbdacat/weather-dashboard",
-          projectLink: "https://tabbdacat.github.io/weather-dashboard/",
-          summary: "Brief summary",
-          description: "Full Description of Project",
-          tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-      },
-      {
-        id: 4,
-          name: "Coding Quiz",
-          pictures: [], 
-          // githubLink: "https://github.com/tabbdacat/coding-quiz",
-          projectLink: "https://tabbdacat.github.io/coding-quiz/",
-          summary: "Brief summary",
-          description: "Full Description of Project",
-          tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-      },
-      {
-        id: 5,
-          name: "Social Network API",
-          pictures: [],
-          // githubLink: "https://github.com/tabbdacat/social-network-api",
-          projectLink: "https://drive.google.com/file/d/1xwfROda1DgFF3f8CBdpaUapTH7MgSlPs/view",
-          summary: "Brief summary",
-          description: "Full Description of Project",
-          tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-      },
-    ];
+  projects = {} as Project[];
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private projectService: ProjectsService) {
     this.titleService.setTitle('Tabbatha Souza - Portfolio');
+  }
+  ngOnInit(): void {
+    this.projects = this.projectService.GetProjects();
   }
 }
