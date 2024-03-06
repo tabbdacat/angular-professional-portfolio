8bcd9cce0d6e73fd021ccdff0acdbee212365786
Meow <tabbathapischke@gmail.com>
2024-03-05 16:27:47 -1000

    add tags to portfolio component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,12 +3,6 @@ import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 import { Tag } from '../_models/Tag'
 import { ProjectsService } from '../_services/projects.service';
-// import { culinaryConnectImg } from '../../assets/culinaryConnectImg.jpg';
-// import { weatherDashImg } from '../../assets/weatherDashImg.jpg';
-// import { techBlogImg } from '../../assets/techBlogImg';
-// import { movieMixImg } from '../../assets/movieMixImg';
-// import { codingQuizImg } from '../../assets/codingQuizImg';
-// import { socialAPIImg } from '../../assets/socialAPIImg';
 
 @Component({
   selector: 'app-portfolio',
@@ -22,6 +16,18 @@ export class PortfolioComponent implements OnInit {
   isCollapsed: boolean = true;
   typeScript: boolean = false;
   angular: boolean = false;
+  javaScript: boolean = false;
+  html: boolean = false;
+  css: boolean = false;
+  jquery: boolean = false;
+  bootstrap: boolean = false;
+  react: boolean = false;
+  nodejs: boolean = false;
+  materialui: boolean = false;
+  restfulapis: boolean = false;
+  mongodb: boolean = false;
+  sql: boolean = false;
+  bulma: boolean = false;
   filtering: boolean = false;
 
   constructor(private titleService: Title, private projectService: ProjectsService) {
@@ -40,7 +46,43 @@ export class PortfolioComponent implements OnInit {
     if (this.angular) {
       filterTags.push(Tag.ANGULAR);
     }
-    if (this.typeScript || this.angular) {
+    if (this.javaScript) {
+      filterTags.push(Tag.JAVASCRIPT);
+    }
+    if (this.html) {
+      filterTags.push(Tag.HTML);
+    }
+    if (this.css) {
+      filterTags.push(Tag.CSS);
+    }
+    if (this.jquery) {
+      filterTags.push(Tag.JQUERY);
+    }
+    if (this.bootstrap) {
+      filterTags.push(Tag.BOOTSTRAP);
+    }
+    if (this.react) {
+      filterTags.push(Tag.REACT);
+    }
+    if (this.nodejs) {
+      filterTags.push(Tag.NODEJS);
+    }
+    if (this.materialui) {
+      filterTags.push(Tag.MATERIALUI);
+    }
+    if (this.restfulapis) {
+      filterTags.push(Tag.RESTFULAPIS);
+    }
+    if (this.mongodb) {
+      filterTags.push(Tag.MONGODB);
+    }
+    if (this.sql) {
+      filterTags.push(Tag.SQL);
+    }
+    if (this.bulma) {
+      filterTags.push(Tag.BULMA);
+    }
+    if (this.typeScript || this.angular || this.javaScript || this.html || this.css || this.jquery || this.bootstrap || this.react || this.nodejs || this.materialui || this.restfulapis || this.mongodb || this.sql || this.bulma) {
       this.filtering = true;
     } else {
       this.filtering = false;
