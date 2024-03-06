8ff00c7b18caf84533f41985384cd0ad92640198
Meow <tabbathapischke@gmail.com>
2024-03-05 16:14:52 -1000

    add new tags in models

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,6 +1,18 @@
 export class Tag {
-    static readonly ANGULAR = new Tag('Angular', 'red');
-    static readonly TYPESCRIPT = new Tag('TypeScript', 'green');
+    static readonly TYPESCRIPT = new Tag('TypeScript', '#50808e');
+    static readonly JAVASCRIPT = new Tag('JavaScript', '#50808e');
+    static readonly HTML = new Tag('HTML', '#fcaa67');
+    static readonly CSS = new Tag('CSS', '#f4a698');
+    static readonly JQUERY = new Tag('jQuery', '#50808e');
+    static readonly BOOTSTRAP = new Tag('Bootstrap', '#f4a698');
+    static readonly REACT = new Tag('React', '#88d9e6 ');
+    static readonly NODEJS = new Tag('NodeJS', '#6d213c');
+    static readonly ANGULAR = new Tag('Angular', '#88d9e6');
+    static readonly MATERIALUI = new Tag('MaterialUI', '#f4a698');
+    static readonly RESTFULAPIS = new Tag('RESTfulAPIs', '#8b8bae');
+    static readonly MONGODB = new Tag('MongoDB', '#62bbc1');
+    static readonly SQL = new Tag('SQL', '#62bbc1');
+    static readonly BULMA = new Tag('Bulma', '#f4a698');
 
     private constructor(private readonly key: string, public readonly color: string) {
 
